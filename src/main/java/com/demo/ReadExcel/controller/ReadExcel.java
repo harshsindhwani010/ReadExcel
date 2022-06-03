8c60658f1acad32e34fd0fcbb6dded0b94432cb5
harshsindhwani010 <harshsindhwani010@gmail.com>
2022-06-03 12:34:49 +0530

    File uploaded in resources folder

--- src/main/java/com/demo/ReadExcel/controller/ReadExcel.java
+++ src/main/java/com/demo/ReadExcel/controller/ReadExcel.java
@@ -3,6 +3,10 @@ package com.demo.ReadExcel.controller;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+import org.httpobjects.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletContext;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,46 +27,63 @@ import java.util.Iterator;
 @Controller
 public class ReadExcel {
 
+    @Autowired
+    ServletContext context;
     @GetMapping("/")
     public String get() {
 
         return "index";
     }
 
-    @PostMapping("/processExcel")
-    public void getExcel(@RequestParam("file") MultipartFile file) throws IOException, InvalidFormatException {
-        StringBuilder sb = new StringBuilder();
-        String fileN = file.getOriginalFilename();
-        File f = new File(fileN);
-        System.out.println("f pa::"+f.getPath());
-        Workbook wb = WorkbookFactory.create(f);
-        System.out.println(wb);
-        Sheet mySheet = wb.getSheetAt(0);
-        Iterator<Row> rowIter = mySheet.rowIterator();
-        for (Iterator<Row> rowIterator = mySheet.rowIterator(); rowIterator.hasNext(); ) {
-            for (Iterator<Cell> cellIterator = ((Row) rowIterator.next()).cellIterator(); cellIterator.hasNext(); ) {
-                sb.append(((Cell) cellIterator.next()).toString());
-                sb.append(",");
-                System.out.print(((Cell) cellIterator.next()).toString());
-                System.out.print(",");
-            }
-            sb.append("\n");
-            System.out.println("***");
-        }
-//            System.out.println("END");
-        //sb.append("Id,name,address");
-        //sb.append("\n");
-        //sb.append("1,harsh,India");
-        //sb.append("\n");
-        //sb.append("2,abhishek,India");
-        //sb.append("\n");
-        Path paths = Paths.get("C:\\Users\\Ditsd\\Downloads\\new1.txt");
-        Files.write(paths, Arrays.asList(sb.toString()));
-
-//        ModelAndView mv = new ModelAndView();
-//        mv.addObject("file",file);
-//        mv.setViewName("inserted");
-//        return mv;
+    @PostMapping("/upload-file")
+    public /*ResponseEntity<String>*/ String getExcel(@RequestParam("file") MultipartFile file) throws IOException, InvalidFormatException {
+        //System.out.println(context.getRealPath("resources"));
+        String project = System.getProperty("user.dir");
+        String path = project+"\\src\\main\\resources";
+        System.out.println(file.getInputStream());
+        InputStream is = file.getInputStream();
+        byte data[] = new byte[is.available()];
+        is.read(data);
+        FileOutputStream fos = new FileOutputStream(path+File.separator+file.getOriginalFilename());
+        fos.write(data);
+        fos.flush();
+        fos.close();
+        //return ResponseEntity.ok("Upload Successfully");
+        return "inserted";
     }
 
+
+//        StringBuilder sb = new StringBuilder();
+//        String fileN = file.getOriginalFilename();
+//        File f = new File(fileN);
+//        System.out.println("f pa::"+f.getPath());
+//        Workbook wb = WorkbookFactory.create(f);
+//        System.out.println(wb);
+//        Sheet mySheet = wb.getSheetAt(0);
+//        Iterator<Row> rowIter = mySheet.rowIterator();
+//        for (Iterator<Row> rowIterator = mySheet.rowIterator(); rowIterator.hasNext(); ) {
+//            for (Iterator<Cell> cellIterator = ((Row) rowIterator.next()).cellIterator(); cellIterator.hasNext(); ) {
+//                sb.append(((Cell) cellIterator.next()).toString());
+//                sb.append(",");
+//                System.out.print(((Cell) cellIterator.next()).toString());
+//                System.out.print(",");
+//            }
+//            sb.append("\n");
+//            System.out.println("***");
+//        }
+////            System.out.println("END");
+//        //sb.append("Id,name,address");
+//        //sb.append("\n");
+//        //sb.append("1,harsh,India");
+//        //sb.append("\n");
+//        //sb.append("2,abhishek,India");
+//        //sb.append("\n");
+//        Path paths = Paths.get("C:\\Users\\Ditsd\\Downloads\\new1.txt");
+//        Files.write(paths, Arrays.asList(sb.toString()));
+//
+////        ModelAndView mv = new ModelAndView();
+////        mv.addObject("file",file);
+////        mv.setViewName("inserted");
+////        return mv;
 }
+
